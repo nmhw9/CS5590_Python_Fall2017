76817069719b93ec68fd9ce83e943dd4d78b3730
nmhw9 <31782325+nmhw9@users.noreply.github.com>
2017-09-08 16:02:08 -0700

    Add files via upload

---
+++ EX1_AreaCircum.py
@@ -0,0 +1,8 @@
+import math
+
+ri = input('what is radius')
+r=float(ri)
+area = math.pi*(r*r)
+circum=2*math.pi*r
+print('area of circle is :',area)
+print('circumference is: ',circum)
---
+++ ex2_OddEndtry.py
@@ -0,0 +1,8 @@
+name = input('what is your name')
+numbi = input('enter any number')
+numb = int(numbi)
+i = numb%2
+if i==0:
+    print("number entered by",name,"is not ODD")
+else :
+    print('number entered by',name,'is ODD')
